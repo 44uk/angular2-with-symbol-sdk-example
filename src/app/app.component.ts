491d9be902fbb58955c9bf757f786a5242dd3a81
Yoshiyuki Ieyama <yukku0423@gmail.com>
2019-11-26 15:10:26 +0900

    add account generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {
+  Account, NetworkType
+} from 'nem2-sdk'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'my-app-with-nem';
+  account: Account
+
+  ngOnInit() {
+    this.account = Account.generateNewAccount(NetworkType.MIJIN_TEST)
+  }
 }
