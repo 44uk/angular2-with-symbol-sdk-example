b6543d0d5d5fd82616dea11e923f69dd3886f37a
Yoshiyuki Ieyama <yukku0423@gmail.com>
2020-08-17 10:48:24 +0900

    update demo implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {
-  Account, NetworkType
+  Account, NetworkType, TransferTransaction, Deadline, EmptyMessage, SignedTransaction
 } from 'symbol-sdk'
 
 
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
 
   title = 'my-app-with-nem';
   account: Account
+  signedTx: SignedTransaction
 
   ngOnInit() {
     this.generateNewAccount()
@@ -21,5 +22,16 @@ export class AppComponent implements OnInit {
 
   generateNewAccount() {
     this.account = Account.generateNewAccount(NetworkType.TEST_NET)
+  }
+
+  createTransaction() {
+    const tx = TransferTransaction.create(
+      Deadline.create(),
+      this.account.address,
+      [],
+      EmptyMessage,
+      NetworkType.TEST_NET
+    ).setMaxFee(500) as TransferTransaction
+    this.signedTx = this.account.sign(tx, "693A04094232B8E1BA275798095B8C7170406BDEEA85B301E9B9B72C1907DC24")
   }
 }
